d1ca0fd8dadca727dd254e041150e6781f4f1b51
3Turr <a.x.l@live.com>
2015-11-28 03:15:10 -0500

    Fix setup

--- linux/setup.sh
+++ linux/setup.sh
@@ -3,4 +3,6 @@ if (( $EUID != 0 )); then
 	echo "You need to run this script as root"
 	exit
 fi
+mv sem.sh /usr/bin/sem
+chmod 755 /usr/bin/sem
 mkdir -p /usr/share/sem/ & cp -r ./* /usr/share/sem/
